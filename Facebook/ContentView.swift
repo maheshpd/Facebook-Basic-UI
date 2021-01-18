4250c0f0792b91e65b4256f54b5394a5af3458d7
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-18 19:31:44 +0530

    Initial Commit

---
+++ Facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook
+//
+//  Created by Mahesh Prasad on 18/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Mahesh Prasad on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
